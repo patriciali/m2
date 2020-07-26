db67d12e71849ba7819196f9c07022cc00d87655
Patricia Li <patricia.li@gmail.com>
2020-07-25 19:32:17 -0700

    make it easier to copy-paste

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from cube import Cube
 import utils
 
 ##################################################
-scramble = "U2 B' U2 L2 R2 F2 D2 R2 F2 U2 R F' L' D B R U B R' U'"
+scramble = '''
+U2 B' U2 L2 R2 F2 D2 R2 F2 U2 R F' L' D B R U B R' U'
+'''
 hint_edge_cycle_starts_list = 'LAT'
 hint_corner_cycle_starts_list = ''
 ##################################################
 
-scramble_arr = scramble.split(' ')
+scramble_arr = scramble.strip().split(' ')
 hint_edge_cycle_starts = set([utils.letter_to_index(letter) for letter in hint_edge_cycle_starts_list])
 hint_corner_cycle_starts = set([utils.letter_to_index(letter) for letter in hint_corner_cycle_starts_list])
 
